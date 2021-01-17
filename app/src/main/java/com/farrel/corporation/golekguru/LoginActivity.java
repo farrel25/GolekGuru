b5a33a225d0b4c916a79af9ad52e302157a4abc8
farrel25 <frl25ap@gmail.com>
2021-01-17 11:27:31 +0700

    login xml, done

--- app/src/main/java/com/farrel/corporation/golekguru/LoginActivity.java
+++ app/src/main/java/com/farrel/corporation/golekguru/LoginActivity.java
@@ -3,12 +3,15 @@ package com.farrel.corporation.golekguru;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // remove status bar view
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
     }
 }
--- app/src/main/java/com/farrel/corporation/golekguru/MainActivity.java
+++ app/src/main/java/com/farrel/corporation/golekguru/MainActivity.java
@@ -2,9 +2,12 @@ package com.farrel.corporation.golekguru;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Pair;
+import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -37,7 +40,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
                 Intent loginActivity = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(loginActivity);
+
+                Pair[] pairs = new Pair[2];
+                pairs[0] = new Pair<View, String>(tvAppName, "trans_app_name");
+                pairs[1] = new Pair<View, String>(tvAppCaption, "trans_caption");
+
+                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this, pairs);
+
+                startActivity(loginActivity, options.toBundle());
                 finish();
             }
         }, 3500);
