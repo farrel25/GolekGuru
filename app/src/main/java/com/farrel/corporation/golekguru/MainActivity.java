0658c0df61d04280889f0a30ca38fe03a7d2d79f
farrel25 <frl25ap@gmail.com>
2021-01-17 10:18:45 +0700

    splash screen , done

--- app/src/main/java/com/farrel/corporation/golekguru/MainActivity.java
+++ app/src/main/java/com/farrel/corporation/golekguru/MainActivity.java
@@ -2,13 +2,44 @@ package com.farrel.corporation.golekguru;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Animation topAnimation, bottomAnimation;
+    TextView tvAppName, tvAppCaption;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // remove status bar view
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        // Animations
+        topAnimation = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnimation = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+        // Hooks
+        tvAppName = findViewById(R.id.splash_app_name);
+        tvAppCaption = findViewById(R.id.splash_app_caption);
+
+        tvAppName.setAnimation(topAnimation);
+        tvAppCaption.setAnimation(bottomAnimation);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent loginActivity = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(loginActivity);
+                finish();
+            }
+        }, 3500);
     }
 }
